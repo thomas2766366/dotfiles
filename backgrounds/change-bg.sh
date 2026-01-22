f8c2f87a0cc959c7a71425f70dbe9048407978ec
Jandeman616 <thomas.vanschaik@student.hu.nl>
2026-01-22 20:51:15 +0100

    Added browser hotkey, and got rid off multiple swaybg instances

--- backgrounds/change-bg.sh
+++ backgrounds/change-bg.sh
@@ -3,4 +3,6 @@ picdir=~/backgrounds
 
 curpic=$(ls $picdir | grep ".jpg" | grep -E -v "current" | shuf -n 1)
 cp $picdir/$curpic $picdir/current.jpg
+
+pkill swaybg
 swaybg -i $picdir/current.jpg -m fill
