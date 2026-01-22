9f8f00d7587c6dfbfa52e475f8a8afaba50696b2
Jandeman616 <thomas.vanschaik@student.hu.nl>
2026-01-22 12:24:50 +0100

    Added swaylock and fixed the random background changer

--- backgrounds/change-bg.sh
+++ backgrounds/change-bg.sh
@@ -3,4 +3,4 @@ picdir=~/backgrounds
 
 curpic=$(ls $picdir | grep ".jpg" | grep -E -v "current" | shuf -n 1)
 cp $picdir/$curpic $picdir/current.jpg
-swaybg -i $picdir/current.jpg
+swaybg -i $picdir/current.jpg -m fill
